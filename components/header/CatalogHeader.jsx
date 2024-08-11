2c9295de192e63ed8576141319bf0afb345be748
wahiditckd <wahid.itckd@gmail.com>
2024-08-11 11:05:14 +0700

    first commit

--- components/header/CatalogHeader.jsx
+++ components/header/CatalogHeader.jsx
@@ -1,18 +1,19 @@
-import Link from "next/link"
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 export default function CatalogHeader() {
-    return (
-        <div className="h-[70px] flex items-center w-full ">
-            <div className="wrapper ">
-                <div className="flex justify-between items-center">
-                    <div>logo</div>
-                    <div className="flex gap-2">
-                        <Link href="/">Home</Link>
-                        <Link href="/">Catalog</Link>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="h-[70px] flex items-center w-full ">
+      <div className="wrapper ">
+        <div className="flex justify-between items-center">
+          <div>logo</div>
+          <div className="flex gap-2">
+            <Link href="/">Home</Link>
+            <Link href="/">Catalog</Link>
+            <Link href="/">Contact</Link>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
